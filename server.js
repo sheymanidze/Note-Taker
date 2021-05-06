e2964bc5d96e64fb46ec0c0df8a6c05887335b55
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-05-06 14:54:13 -0400

    db fixed, notes fixed

--- server.js
+++ server.js
@@ -3,71 +3,67 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const PORT = process.env.PORT || 3000;
-//const bodyParser = require('body-parser');
-const db = require('./db/db.json');
 
-
-const { dirname } = require('node:path');
-const { json } = require('express');
-
-
-// //root object
-// var rootObj = { root: _dirname + '/public' };
-// app.get('/', (req, res) => {
-//   res.sendFile('/index.html, rootObj');
-// });
-//app.use(bodyParser.urlencoded({extended: false}));
 
 //access to req.body
 app.use(express.urlencoded({ extended: true }));
-app.use(ecpress.json());
+
+app.use(express.json());
 
 
 //access to public folder
-app.use(express.static(path.join(_dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 //to index.js
 app.get('/assets/js/index.js'), function (req, res) {
-  res.sendFile(path.join(_dirname, 'piblic/assets/js/index.js'))
+  res.sendFile(path.join(__dirname, 'piblic/assets/js/index.js'))
 };
 
-//to index.html
-app.get('*', function (req, res) {
-  res.sendFile(path.join(_dirnmae, 'public/index.html'))
-});
 
 //to notes.html
 app.get('/notes', function (req, res) {
-  res.sendFile(path.join(_dirname, 'public/notes.html'))
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
 
-app.get('api/notes', (req, res) => {
+
+//see clicked notes
+app.get('/api/notes', (req, res) => {
   console.log('/api/notesget');
   let json = getJson();
   console.log(json);
   res.json(json);
 })
 
-app.post('api/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
   console.log('/api/notespost');
   console.log(req.body);
   addNotes(req.body);
   res.json(getJson());
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  console.log('/api/notes/:iddelete')
-  deleteNotes(req.params.id);
-  res.json(getJson());
-})
+
+//deleting/hiding notes
+// app.delete('/api/notes/:id', (req, res) => {
+//   console.log('/api/notes/:iddelete')
+//   console.log(req.params.id)
+//   deleteNotes(req.params.id);
+//   res.json(getJson());
+// })
+
+
+
+//to index.html
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+});
 
 
 // Listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 
 function getJson() {
-  let data = fs.readFileSync(_dirname + db);
+  let data = fs.readFileSync(__dirname + '/db/db.json');
   let json = JSON.parse(data);
   return json;
 };
@@ -81,20 +77,23 @@ function addNotes(notes) {
 
 function saveNotes(jsonData) {
   let data = JSON.stringify(jsonData);
-  json.push(newNotes);
-  saveNotes(json);
+  fs.writeFileSync(__dirname + '/db/db.json', data);
 };
 
-function deleteNotes(id) {
-  let json = getJson();
-  json[id].hide = true;
-  saveNotes(json);
-};
+
+//hiding the notes
+// function deleteNotes(id) {
+//   let json = getJson();
+//   json[id].hide = true;
+//   saveNotes(json);
+// };
 
 function notesTemplate(data) {
+  const randomNumber = Math.floor((Math.random() * 50) + 1);
   let obj = {
     title: data.title,
-    text: data.text
+    text: data.text,
+    id: randomNumber
   }
   return obj
 
